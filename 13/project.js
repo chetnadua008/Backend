c8779088a6087a2e99948d42a8b43872d5c54305
chetnadua008 <chetnadua36@gmail.com>
2025-09-01 12:45:03 +0530

    restapi project mockaroo

---
+++ 13/project.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const users = require("./MOCK_DATA.json")
+const PORT_NUMBER=8000
+
+const app = express()
+//browser
+app.get("/users",(req,res)=>{
+    const html = `
+    <ul>
+        ${users.map((user)=>`<li>${user.email}</li>`).join("")}
+    </ul>
+    `
+    return res.send(html)
+})
+//api
+app.get("/api/users",(req,res)=>{
+    return res.send(users)
+})
+//single user 
+app.get("/users/:id",(req,res)=>{
+    const id = Number(req.params.id)
+    const html = `
+    <div>Hi, ${users.find((user)=>user.id===id).first_name}</div>
+    `
+    return res.send(html)
+})
+//same routes
+app.route("/api/users/:id").get((req,res)=>{
+    const id = Number(req.params.id)
+    const user = users.find((user)=>user.id===id)
+    return res.json(user)
+}).patch((req,res)=>{
+      return res.send(`${req.params.id} user updated`)
+}).delete((req,res)=>{
+    return res.send(`${req.params.id} user deleted`)
+})  
+ 
+app.post("/users",(req,res)=>{
+    return res.send("New user added")
+})
+
+// app.get("/api/users/:id",(req,res)=>{
+    
+// })
+// app.patch("/api/users/:id",(req,res)=>{
+  
+// })
+// app.delete("/api/users/:id",(req,res)=>{
+    
+// })
+app.listen(PORT_NUMBER,()=>{
+    console.log(`Server Started at PORT: ${PORT_NUMBER}`)
+})
+
